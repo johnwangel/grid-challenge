912024f58316ada3db5ee1d1dac6be0f3f202034
John Atkins <johnatkins1999@yahoo.com>
2017-06-19 11:44:26 -1000

    removes items if less

--- maxtrix.js
+++ maxtrix.js
@@ -7,22 +7,43 @@ function make2DArray(rows, columns, fill){
           matrix[i].push([fill]);
       }
   }
-
   return matrix;
 }
 
 firstArray = make2DArray(5, 10, "ham");
 
-function expand2DArray(firstArray, rows, columns, fill){
-  for (var i = 0; i < rows; i++) {
-      if (firstArray[i] == undefined) { firstArray.push([]); }
-      for (var j = firstArray[i].length; j < columns; j++) {
-          if (firstArray[i][j] == undefined) { firstArray[i].push([fill]); }
-      }
+function expand2DArray(rows, columns, fill, array){
+  if (array == undefined ){ let array = []; }
+
+  let len1 = (function() {
+    if (array.length > rows) { return array.length; }
+      else { return rows; }
+  })();
+  for (var i = 0; i < len1; i++) {
+    if (i > rows-1) {
+      array.pop();
+      continue;
+    }
+    else if (array[i] == undefined) {
+      array.push([]);
+    }
+    let len2 = (function() {
+      if (array[i] && array[i].length > columns) { return array[i].length; }
+        else { return rows; }
+    })();
+    for (var j = 0; j < len2; j++) {
+      if (j > columns-1) { array[i].pop(); }
+      else if (array[i][j]) {}
+      else { array[i].push([fill]); }
+    }
   }
-  return firstArray;
+  return array;
 }
 
-secondArray = expand2DArray(firstArray, 10, 15, "spam");
+//secondArray = expand2DArray(firstArray, 10, 15, "spam");
 
+//console.log(secondArray);
+
+secondArray = expand2DArray(15, 15, "spam", firstArray);
+secondArray = expand2DArray(3, 3, "spam", firstArray);
 console.log(secondArray);
