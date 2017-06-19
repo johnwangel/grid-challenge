0e1bdeb49eab1a01f2d43888dc29eb6bc33b5aba
John Atkins <johnatkins1999@yahoo.com>
2017-06-19 10:12:22 -1000

    stretch

--- maxtrix.js
+++ maxtrix.js
@@ -1,16 +1,32 @@
 
-function makeArray(rows, columns){
+function make2DArray(rows, columns, fill){
   let matrix = [];
   for (var i = 0; i < rows; i++) {
       matrix.push([]);
       for (var j = 0; j < columns; j++) {
-          matrix[i].push([j+1]);
+          matrix[i].push([fill]);
       }
   }
 
   return matrix;
-
-  console.log(matrix);
 }
 
-console.log(makeArray(5,10));
+firstArray = make2DArray(5, 10, "spam");
+
+function expand2DArray(firstArray, rows, columns, fill){
+  let newRows = rows - firstArray.length;
+
+  for (var i = firstArray.length; i < rows; i++) {
+      firstArray.push([]);
+      let newCols = columns - firstArray[i].length;
+      for (var j = firstArray[i].length; j < columns; j++) {
+          firstArray[i].push([fill]);
+      }
+  }
+
+  return firstArray;
+}
+
+secondArray = expand2DArray(firstArray, 10, 15, "ham");
+
+console.log(secondArray);
